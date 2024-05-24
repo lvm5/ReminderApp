3b95316eaa95027ff925d718629f82bcbecc2e64
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2024-05-24 17:04:02 -0300

    Initial Commit

---
+++ ReminderApp/ReminderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppApp.swift
+//  ReminderApp
+//
+//  Created by Leandro Morais on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
